532741fa1273778c0c0dcb9f42a14fb504add9d2
Baltazarky <61981650+Baltazarky@users.noreply.github.com>
2023-06-05 20:51:32 -0300

    Todo listo para primer entrega

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,28 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Loader, Sprite } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	width: 920,
+	height: 920
 });
 
-const clampy: Sprite = Sprite.from("clampy.png");
+Loader.shared.add({url: "./homero.png", name: "homero"});
 
-clampy.anchor.set(0.5);
+Loader.shared.onComplete.add(()=>{
+	const homero: Sprite = Sprite.from("homero");
 
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
+	console.log("Holiwis!", homero.width, homero.height);
 
-app.stage.addChild(clampy);
+	homero.anchor.set(0.5);
+
+	homero.x = app.screen.width / 2;
+	homero.y = app.screen.height / 2;
+
+	app.stage.addChild(homero);
+});
+
+Loader.shared.load();
+
